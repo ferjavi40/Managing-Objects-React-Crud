8fe33d2d4ddcf9a9a8442cd53a8b2d4a6d77321e
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-01-22 15:32:57 +0100

    setting firebase on app

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAoBGSTF8t54KIQRsS7XNLfQyJi6eSL5dU",
+    authDomain: "manage-crud-react.firebaseapp.com",
+    projectId: "manage-crud-react",
+    storageBucket: "manage-crud-react.appspot.com",
+    messagingSenderId: "278031926646",
+    appId: "1:278031926646:web:5e723d9cf137dcfe736657"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//firebase
+import './firebase'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
